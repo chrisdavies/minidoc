09cb43626b060ddd47ecfa59076b0163990f333b
Christopher Davies <github@christophilus.com>
2020-12-10 13:25:53 -0500

    Make drag / drop reordering of cards smoother.

--- src/card/draggable.ts
+++ src/card/draggable.ts
@@ -2,7 +2,10 @@ import * as Dom from '../dom';
 import { h } from '../dom';
 import { debounce } from '../util';
 
-let draggingEl: Element | undefined;
+// We show a dragging line in the editor, so we'll use a transparent gif to hide the drag image,
+// which I find to be more of a distraction than a help.
+const dragImg = new Image(1, 1);
+dragImg.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 
 /**
  * Make the specified element draggable, and understood by this drop tracker.
@@ -10,15 +13,6 @@ let draggingEl: Element | undefined;
  */
 export function makeDraggable(el: Element) {
   el.setAttribute('draggable', 'true');
-  el.addEventListener('dragstart', (e: any) => {
-    const tmp = el as HTMLElement;
-    const opacity = tmp.style.opacity;
-    tmp.style.opacity = '0.2';
-    setTimeout(() => (tmp.style.opacity = opacity), 10);
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text', 'mini-card');
-    draggingEl = Dom.findLeaf(el);
-  });
   return el;
 }
 
@@ -26,50 +20,68 @@ export function makeDraggable(el: Element) {
  * Enable drag / drop reordering for direct, draggable children of the specified element.
  */
 export function enableDragDrop(el: Element) {
+  // The element being dragged
+  let draggingEl: Element | undefined;
+
+  // The drop target preview, showing where draggingEl will be placed when drag completes
   const dropTarget = h<HTMLElement>('drop-target.minidoc-drop-target');
-  let prevY = 0;
 
-  const repositionDropTarget = debounce((e: any) => {
-    if (!draggingEl || e.target.tagName === 'DROP-TARGET') {
-      return;
-    }
-    const leaf = Dom.findLeaf(e.target);
-    if (!leaf) {
+  const repositionDropTarget = debounce((e: DragEvent) => {
+    if (!draggingEl) {
       return;
     }
 
-    const clientY = e.clientY;
-
-    if (!draggingEl.contains(e.target) && prevY !== clientY) {
-      const aboveEl = prevY >= clientY ? leaf : leaf.nextElementSibling || undefined;
-      if (aboveEl) {
-        aboveEl.parentElement?.insertBefore(draggingEl, aboveEl);
-      } else {
-        el.append(draggingEl);
+    // Find the elment above which we should insert the drop target. If the mouse
+    // is above the midpoint of an element, we'll place the drop target above
+    // otherwise, we'll put it below.
+    // This is pretty inefficient, but accurate. If we find it's a perf problem
+    // on old machines, we can cache the element sizes once when drag starts.
+    let above: Element | null = el.firstElementChild;
+    above?.clientTop;
+    while (above) {
+      const bounds = above.getBoundingClientRect();
+      if (bounds.top + bounds.height / 2 > e.clientY) {
+        break;
       }
+      above = above.nextElementSibling;
     }
 
-    prevY = clientY;
-    if (!dropTarget.isConnected) {
-      const leafBounds = draggingEl.getBoundingClientRect();
-      dropTarget.style.width = `${leafBounds.width}px`;
-      dropTarget.style.left = `${leafBounds.left}px`;
-      dropTarget.style.top = `${leafBounds.top - 2}px`;
-      dropTarget.style.height = `${leafBounds.height + 4}px`;
-      (draggingEl as HTMLElement).style.visibility = 'hidden';
-      document.body.appendChild(dropTarget);
-    } else {
-      dropTarget.style.top = `${draggingEl.getBoundingClientRect().top - 2}px`;
-    }
+    el.insertBefore(dropTarget, above);
   }, 50);
+
+  el.addEventListener('dragstart', (e: any) => {
+    draggingEl = Dom.findLeaf(e.target);
+    if (!draggingEl) {
+      return;
+    }
+
+    // We attach event listeners to the document because it's pretty easy to
+    // drag the element outside of the editor, but you still want to update the
+    // drop position even so.
+    const done: Array<() => void> = [];
+    const off = () => done.forEach((f) => f());
+    done.push(
+      Dom.on(document, 'dragover', (e) => {
+        e.preventDefault();
+        repositionDropTarget(e);
+      }),
+    );
+    done.push(Dom.on(document, 'dragend', off));
+    done.push(Dom.on(document, 'mousemove', off));
+
+    e.dataTransfer!.setData('text', 'mini-card');
+    e.dataTransfer!.setDragImage(dragImg, 0, 0);
+    (draggingEl as HTMLElement).style.opacity = '0.2';
+  });
 
   function finishDrag(e: Event) {
     e.preventDefault();
-    dropTarget.remove();
     if (draggingEl) {
-      (draggingEl as HTMLElement).style.visibility = 'visible';
+      dropTarget.isConnected && dropTarget.replaceWith(draggingEl);
+      (draggingEl as HTMLElement).style.opacity = '1';
       draggingEl = undefined;
     }
+    dropTarget.remove();
   }
 
   el.addEventListener('dragover', (e) => {
@@ -82,10 +94,6 @@ export function enableDragDrop(el: Element) {
     repositionDropTarget(e);
   });
 
-  dropTarget.addEventListener('dragover', (e) => e.preventDefault());
-
   el.addEventListener('dragend', finishDrag);
   el.addEventListener('drop', finishDrag);
-  dropTarget.addEventListener('dragend', finishDrag);
-  dropTarget.addEventListener('drop', finishDrag);
 }
