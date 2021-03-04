d74fcdfe325e24fc93f1372dfd6e13754a186628
Christopher Davies <dev@christophilus.com>
2021-03-04 13:32:49 -0500

    Fix placeholder text in lis. Fixes #11

--- src/placeholder/placeholder.ts
+++ src/placeholder/placeholder.ts
@@ -5,10 +5,14 @@ import { EditorMiddleware } from '../types';
  * Attach placeholder support to the specified editor.
  */
 export const placeholder = (placeholder: string): EditorMiddleware => (next, editor) => {
-  const el = editor.root;
+  const el: HTMLElement = editor.root;
   const updatePlaceholder = () => {
     const placeholderText =
-      (el.childElementCount <= 1 && Dom.isEmpty(el, true) && placeholder) || '';
+      (el.childElementCount <= 1 &&
+        Dom.isEmpty(el, true) &&
+        !Dom.isList(el.firstElementChild) &&
+        placeholder) ||
+      '';
     if (placeholderText !== el.getAttribute('placeholder')) {
       el.setAttribute('placeholder', placeholderText);
     }
