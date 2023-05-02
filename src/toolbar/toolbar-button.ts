aacdff5ef5341a78416161236b012351051fdb7c
Halil Bilir <hbilir@gmail.com>
2023-05-02 16:34:48 +0300

    Focus back to the editor when a toolbar button is clicked. (#33)
    * Pass onmousedown methods to toolbar buttons.

--- src/toolbar/toolbar-button.ts
+++ src/toolbar/toolbar-button.ts
@@ -3,13 +3,14 @@ import { MinidocToolbarAction, MinidocToolbarEditor } from './toolbar-types';
 
 export function ToolbarButton(
   editor: MinidocToolbarEditor,
-  { label, isActive, html, run }: Pick<MinidocToolbarAction, 'label' | 'isActive' | 'html' | 'run'>,
+  { label, isActive, html, run, onMouseDown }: Pick<MinidocToolbarAction, 'label' | 'isActive' | 'html' | 'run' | 'onMouseDown'>,
 ) {
   const btn = h('button.minidoc-toolbar-btn', {
     refreshState:
       isActive &&
       ((editor: MinidocToolbarEditor) =>
         btn.classList.toggle('minidoc-toolbar-btn-active', isActive(editor))),
+    onmousedown: onMouseDown,
     onclick() {
       run(editor);
     },
