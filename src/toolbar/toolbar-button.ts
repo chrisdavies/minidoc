d5556d593a5c431b9ecd40e6c49fbd15db3a207d
Halil Bilir <hbilir@gmail.com>
2023-05-02 11:40:45 +0300

    Focus back to the editor when a toolbar button is clicked.

--- src/toolbar/toolbar-button.ts
+++ src/toolbar/toolbar-button.ts
@@ -5,13 +5,18 @@ export function ToolbarButton(
   editor: MinidocToolbarEditor,
   { label, isActive, html, run }: Pick<MinidocToolbarAction, 'label' | 'isActive' | 'html' | 'run'>,
 ) {
+  let lastActive: Element | null;
   const btn = h('button.minidoc-toolbar-btn', {
     refreshState:
       isActive &&
       ((editor: MinidocToolbarEditor) =>
         btn.classList.toggle('minidoc-toolbar-btn-active', isActive(editor))),
+    onmousedown: () => {
+      lastActive = document.activeElement;
+    },
     onclick() {
       run(editor);
+      (lastActive as HTMLElement)?.focus();
     },
     type: 'button',
     'aria-label': label,
